27c4cdc3fc3607894b3ae0a070c189352959f071
Ferenc Besenyei <ferencbesenyei@protonmail.com>
2024-04-21 09:16:49 +0200

    updated two crystal balls tests

--- src/tests/two_crystal_balls.test.ts
+++ src/tests/two_crystal_balls.test.ts
@@ -1,14 +1,29 @@
 import { twoCrystalBalls } from '../problems/two_crystal_balls';
 
-test('Crystal Balls', () => {
-  const numOfLevels = 10_000;
-  const idx = Math.floor(Math.random() * numOfLevels);
-  const data = new Array(numOfLevels).fill(false) as boolean[];
+describe('Crystal Balls', () => {
+  test('random', () => {
+    const numOfLevels = 10_000;
+    const idx = Math.floor(Math.random() * numOfLevels);
+    const data = new Array(numOfLevels).fill(false) as boolean[];
 
-  for (let i = idx; i < numOfLevels; ++i) {
-    data[i] = true;
-  }
+    for (let i = idx; i < numOfLevels; ++i) {
+      data[i] = true;
+    }
 
-  expect(twoCrystalBalls(data)).toEqual(idx);
-  expect(twoCrystalBalls(new Array(821).fill(false) as boolean[])).toEqual(-1);
+    expect(twoCrystalBalls(data)).toEqual(idx);
+  });
+
+  test('array only contains false', () => {
+    expect(twoCrystalBalls(new Array(821).fill(false) as boolean[])).toEqual(
+      -1,
+    );
+  });
+
+  test('last element is the only true value', () => {
+    const array = new Array(123).fill(false) as boolean[];
+
+    array[122] = true;
+
+    expect(twoCrystalBalls(array)).toEqual(122);
+  });
 });
