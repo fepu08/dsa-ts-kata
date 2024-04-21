72e50cd1c69a0e663f048f55cdab473ddf1e50f3
Ferenc Besenyei <ferencbesenyei@protonmail.com>
2024-04-21 08:52:29 +0200

    bubble sort solution

--- src/algorithms/bubble_sort.ts
+++ src/algorithms/bubble_sort.ts
@@ -1,3 +1,18 @@
+import { swap } from '../utils';
+
 export function bubbleSort(array: number[]): number[] {
+  let swapped = false;
+  for (let i = 0; i < array.length - 1; i++) {
+    for (let j = 0; j < array.length - 1 - i; j++) {
+      console.log(JSON.stringify(array));
+
+      if (array[j] > array[j + 1]) {
+        swap(array, j, j + 1);
+        swapped = true;
+      }
+    }
+    if (swapped == false) break;
+  }
+
   return array;
 }
