ee70101317b8fc5a99692e489c337a4e9bc5aace
Ferenc Besenyei <ferencbesenyei@protonmail.com>
2024-04-21 08:52:16 +0200

    binary search solution

--- src/algorithms/binary_search.ts
+++ src/algorithms/binary_search.ts
@@ -1,3 +1,18 @@
 export function binarySearch(array: number[], item: number): boolean {
+  let low = 0;
+  let high = array.length;
+  let mid = Math.floor(high / 2);
+
+  while (low < high) {
+    if (array[mid] === item) {
+      return true;
+    } else if (item > array[mid]) {
+      low = mid + 1;
+    } else {
+      high = mid;
+    }
+    mid = Math.floor(low + (high - low) / 2);
+  }
+
   return false;
 }
