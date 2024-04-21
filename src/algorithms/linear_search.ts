d3a55ef2b1d9ead5e57a8c153b9f9b180182022d
Ferenc Besenyei <ferencbesenyei@protonmail.com>
2024-04-21 08:52:05 +0200

    linear search solution

--- src/algorithms/linear_search.ts
+++ src/algorithms/linear_search.ts
@@ -1,3 +1,8 @@
 export function linearSearch(array: number[], item: number): boolean {
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] === item) {
+      return true;
+    }
+  }
   return false;
 }
