2f6e974f9905664f0a5f6c2568749b46a0694465
Ferenc Besenyei <ferencbesenyei@protonmail.com>
2024-04-21 08:54:17 +0200

    removed solution of two crystal balls problem

--- src/problems/two_crystal_balls.ts
+++ src/problems/two_crystal_balls.ts
@@ -3,19 +3,5 @@
 	determine the exact spot in which it will break in the most optimized way
 */
 export function twoCrystalBalls(breaks: boolean[]): number {
-  const jumpAmount = Math.floor(Math.sqrt(breaks.length));
-  let i = jumpAmount;
-  for (; i < breaks.length; i += jumpAmount) {
-    if (breaks[i]) {
-      break;
-    }
-  }
-
-  i -= jumpAmount;
-  for (let j = 0; j < jumpAmount && i < breaks.length; j++, i++) {
-    if (breaks[i]) {
-      return i;
-    }
-  }
   return -1;
 }
