1b86fb786089365f7281c06fb57cb688406c2afd
Ferenc Besenyei <ferencbesenyei@protonmail.com>
2024-05-05 10:23:29 +0200

    added dfs on binary search tree solution

--- src/algorithms/dfs_on_binary_search_tree.ts
+++ src/algorithms/dfs_on_binary_search_tree.ts
@@ -1,5 +1,21 @@
 import { BinaryNode } from '../global';
 
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+  if (!curr) {
+    return false;
+  }
+
+  if (curr.value === needle) {
+    return true;
+  }
+
+  if (curr.value < needle) {
+    return search(curr.right, needle);
+  }
+
+  return search(curr.left, needle);
+}
+
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-  return true;
+  return search(head, needle);
 }
